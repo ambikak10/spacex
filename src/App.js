4bf751438814788d604dd38c870ed55e27d03175
Ambika K <ambikak10@gmail.com>
2021-01-10 22:53:32 -0800

    removed <Landing /> in app.js and made as route

--- src/App.js
+++ src/App.js
@@ -12,10 +12,9 @@ function App() {
     <Provider store={store}>
       <div className='App-header'>
         <Router>
-          {/* <Route exact path='/' component={Landing} /> */}
+          <Route exact path='/' component={Landing} />
           
         </Router>
-        <Landing />
       </div>
       <Footer />
     </Provider>
